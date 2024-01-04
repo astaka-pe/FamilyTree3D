03ff7dc71e88181e4545d09ef1b98d7e338b53a6
astaka-pe <astaka1119@g.ecc.u-tokyo.ac.jp>
2024-01-04 14:12:46 +0900

    :boom: support blender viewer

--- family_tree.py
+++ family_tree.py
@@ -1,44 +1,80 @@
-import pygraphviz as pgv
+# import pygraphviz as pgv
+import numpy as np
 from collections import deque
 
 class FamilyTree:
     def __init__(self, center):
         self.center = center
-    
-    def build_graph(self):
-        self.G = pgv.AGraph(directed=True)
         self.person_depth = {self.center: 0}
-        self.build_descend()
-        self.build_ancestor()
+        self.compute_root()
+        self.compute_positions()
     
-    def build_whole_graph(self):
-        self.G = pgv.AGraph(directed=True)
-        self.person_depth = {self.center: 0}
-        self.build_ancestor()
-        for person, dep in self.roots:
-            self.center = person
-            self.build_descend()
+    # def build_graph(self):
+    #     G = pgv.AGraph(directed=True)
+    #     self.person_depth = {self.center: 0}
+    #     G = self.build_descend(G)
+    #     G = self.build_ancestor(G)
+    #     return G
     
-    def build_descend(self):
-        queue = [self.center]
-        person_depth = self.person_depth
-        while queue:
-            if queue[0].partner:
-                self.G.add_edge(queue[0].name, queue[0].partner.name)
-                self.G.add_edge(queue[0].partner.name, queue[0].name)
-                person_depth[queue[0].partner] = person_depth[queue[0]]
-                queue = queue + queue[0].children
-                for child in queue[0].children:
-                    self.G.add_edge(queue[0].name, child.name)
-                    self.G.add_edge(queue[0].partner.name, child.name)
-                    person_depth[child] = person_depth[queue[0]] + 1
-                # print([q.name for q in queue])
-                queue = queue[1:]
-            else:
-                queue = queue[1:]
-        self.person_depth = person_depth
+    # def build_whole_graph(self):
+    #     G = pgv.AGraph(directed=True)
+    #     self.person_depth = {self.center: 0}
+    #     self.build_ancestor(G)
+    #     for person, dep in self.roots:
+    #         self.center = person
+    #         G = self.build_descend(G)
+    #     return G
     
-    def build_ancestor(self):
+    # def build_descend(self, G):
+    #     queue = [self.center]
+    #     person_depth = self.person_depth
+    #     while queue:
+    #         if queue[0].partner:
+    #             G.add_edge(queue[0].name, queue[0].partner.name)
+    #             G.add_edge(queue[0].partner.name, queue[0].name)
+    #             person_depth[queue[0].partner] = person_depth[queue[0]]
+    #             queue = queue + queue[0].children
+    #             for child in queue[0].children:
+    #                 G.add_edge(queue[0].name, child.name)
+    #                 G.add_edge(queue[0].partner.name, child.name)
+    #                 person_depth[child] = person_depth[queue[0]] + 1
+    #             queue = queue[1:]
+    #         else:
+    #             queue = queue[1:]
+    #     self.person_depth = person_depth
+    #     return G
+    
+    # def build_ancestor(self, G):
+    #     queue = [self.center]
+    #     person_depth = self.person_depth
+    #     roots = []
+    #     while queue:
+    #         father = queue[0].father
+    #         mother = queue[0].mother
+    #         if father and mother:
+    #             queue.append(father)
+    #             queue.append(mother)
+    #             G.add_edge(queue[0].father.name, queue[0].name)
+    #             G.add_edge(queue[0].mother.name, queue[0].name)
+    #             G.add_edge(queue[0].father.name, queue[0].mother.name)
+    #             G.add_edge(queue[0].mother.name, queue[0].father.name)
+    #             person_depth[father] = person_depth[queue[0]] - 1
+    #             person_depth[mother] = person_depth[queue[0]] - 1
+    #         else:
+    #             roots.append((queue[0], person_depth[queue[0]]))
+    #         queue = queue[1:]
+    #     self.person_depth = person_depth
+    #     self.roots = roots
+    #     return G
+    
+    def find_siblings(self, person):
+        if person.father and person.mother:
+            siblings = person.father.children
+        else:
+            siblings = []
+        return siblings
+    
+    def compute_root(self):
         queue = [self.center]
         person_depth = self.person_depth
         roots = []
@@ -48,10 +84,6 @@ class FamilyTree:
             if father and mother:
                 queue.append(father)
                 queue.append(mother)
-                self.G.add_edge(queue[0].father.name, queue[0].name)
-                self.G.add_edge(queue[0].mother.name, queue[0].name)
-                self.G.add_edge(queue[0].father.name, queue[0].mother.name)
-                self.G.add_edge(queue[0].mother.name, queue[0].father.name)
                 person_depth[father] = person_depth[queue[0]] - 1
                 person_depth[mother] = person_depth[queue[0]] - 1
             else:
@@ -60,28 +92,21 @@ class FamilyTree:
         self.person_depth = person_depth
         self.roots = roots
     
-    def find_siblings(self, person):
-        if person.father and person.mother:
-            siblings = person.father.children
-        else:
-            siblings = []
-        return siblings
-    
-    def compute_rank(self):
-        pass
-    
     def compute_positions(self):
         roots = self.roots
         person_position_list = []
+        node_list = []
         for root in roots:
             person, dep = root
             root_position = (person, (0, 0, -dep))
-            person_position = self.compute_descend_positions(root_position)
+            person_position, nodes = self.compute_descend_positions(root_position)
             person_position_list.append(person_position)
+            node_list.append(nodes)
         self.person_position_list = person_position_list
         
         self.person_position = {}
-        for per_pos in person_position_list:
+        self.node_list = set()
+        for i, per_pos in enumerate(person_position_list):
             disp_x = 0
             disp_y = 0
             trans = False
@@ -96,15 +121,25 @@ class FamilyTree:
                 for per, pos in per_pos.items():
                     if not per in self.person_position:
                         self.person_position[per] = (pos[0]+disp_x, pos[1]+disp_y, pos[2])
+                for node in node_list[i]:
+                    node_pos = node["location"]
+                    node["location"] = (node_pos[0]+disp_x, node_pos[1]+disp_y, node_pos[2])
             else:
                 for per, pos in per_pos.items():
                     if not per in self.person_position:
                         self.person_position[per] = pos
+        
+            for node in node_list[i]:
+                px, py, pz = node["location"]
+                rx, ry, rz = node["rotation"]
+                sx, sy, sz = node["scale"]
+                self.node_list.add((px, py, pz, rx, ry, rz, sx, sy, sz))
                     
     
     def compute_descend_positions(self, root_position):
         queue = [root_position[0]]
         person_position = {root_position[0]: root_position[1]}
+        node_list = []
         while queue:
             pos = person_position[queue[0]]
             if queue[0].partner:
@@ -114,17 +149,40 @@ class FamilyTree:
                     res = 1                    
                 if pos[2] % 2:
                     p_pos = (pos[0]+res, pos[1], pos[2])
+                    node_pos = (pos[0]+res/2, pos[1], pos[2])
+                    node_rot = (0, np.pi/2, 0)
                 else:
                     p_pos = (pos[0], pos[1]+res, pos[2])
+                    node_pos = (pos[0], pos[1]+res/2, pos[2])
+                    node_rot = (np.pi/2, 0, 0)
                 person_position[queue[0].partner] = p_pos
+                node_list.append({"location": node_pos, "rotation": node_rot, "scale": (0.05, 0.05, 1.0)})
 
                 queue = queue + queue[0].children
+                if len(queue[0].children) > 1:
+                    num_children = len(queue[0].children)
+                    if pos[2] % 2:
+                        node_pos = ((pos[0]+p_pos[0])/2+(num_children-1)/2, pos[1], pos[2]-0.5)
+                        node_rot = (0, np.pi/2, 0)
+                    else:
+                        node_pos = (pos[0], (pos[1]+p_pos[1])/2+(num_children-1)/2, pos[2]-0.5)
+                        node_rot = (np.pi/2, 0, 0)
+                    node_list.append({"location": node_pos, "rotation": node_rot, "scale": (0.05, 0.05, (num_children-1))})
                 for i, child in enumerate(queue[0].children):
                     if pos[2] % 2:
                         person_position[child] = ((pos[0]+p_pos[0])/2+i, pos[1], pos[2]-1)
                     else:
                         person_position[child] = (pos[0], (pos[1]+p_pos[1])/2+i, pos[2]-1)
+                    if i == 0:
+                        node_pos = (person_position[child][0], person_position[child][1], pos[2]-0.5)
+                        node_rot = (0, 0, 0)
+                        node_scale = (0.05, 0.05, 1.0)
+                    else:
+                        node_pos = (person_position[child][0], person_position[child][1], pos[2]-0.75)
+                        node_rot = (0, 0, 0)
+                        node_scale = (0.05, 0.05, 0.5)
+                    node_list.append({"location": node_pos, "rotation": node_rot, "scale": node_scale})
                 queue = queue[1:]
             else:
                 queue = queue[1:]
-        return person_position
+        return person_position, node_list
